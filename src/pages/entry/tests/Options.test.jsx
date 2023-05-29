b7e663f3f8e8ba845a3c0abeb5ea462856f5fedb
Song-A <songa.lee@hutom.io>
2023-05-29 23:33:29 +0900

    Code and test topping options images from server

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -12,3 +12,22 @@ test("displays image for each scoop option from server", async () => {
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });
+
+test("displays image for each toppings option from server", async () => {
+  // Mock Server Worker will return three toppings from server
+  render(<Options optionType="toppings" />);
+
+  // find images, expect 3 based on what msw returns
+  const toppingImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  // confirm alt text of images
+  const altText = toppingImages.map((img) => img.alt);
+  expect(altText).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
+});
